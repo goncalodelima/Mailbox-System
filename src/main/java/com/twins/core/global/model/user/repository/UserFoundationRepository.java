7518a529f386ce4e4b8bab30cd1a5651e4ea5feb
Gonçalo Lima <108634909+goncalodelima@users.noreply.github.com>
2025-05-13 18:40:09 +0100

    Update UserFoundationRepository.java

--- src/main/java/com/twins/core/global/model/user/repository/UserFoundationRepository.java
+++ src/main/java/com/twins/core/global/model/user/repository/UserFoundationRepository.java
@@ -1,9 +1,11 @@
 package com.twins.core.global.model.user.repository;
 
+import com.twins.core.global.model.mail.Mailbox;
 import com.twins.core.global.model.user.GlobalUser;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public interface UserFoundationRepository {
@@ -12,9 +14,15 @@ public interface UserFoundationRepository {
 
     void deleteExpiredBuffs();
 
+    void updateNickname(UUID uuid, String nickname);
+
     CompletableFuture<List<Object[]>> insertOrUpdate(Collection<GlobalUser> globalUsers);
 
-    GlobalUser findOne(String nickname);
+    List<Object[]> insertOrUpdateOnDisable(Collection<GlobalUser> globalUsers);
+
+    GlobalUser findOne(UUID uuid) throws Exception;
+
+    CompletableFuture<Boolean> addMailboxAsync(UUID uuid, Mailbox mailbox);
 
     List<Object[]> findTop();
 
